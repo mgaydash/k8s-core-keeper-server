1ca089b917904d77fc124d5135c78d4f53ee5f4c
mgaydash <mitchell@gaydash.net>
2024-09-17 12:18:25 -0400

    Pull environment variables from secret. Externalize Discord URL

--- install.sh
+++ install.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 usage() {
-  echo "S3_ADMIN_AWS_ACCESS_KEY_ID and S3_ADMIN_AWS_SECRET_ACCESS_KEY should be set"
+  echo "S3_ADMIN_AWS_ACCESS_KEY_ID=${S3_ADMIN_AWS_ACCESS_KEY_ID}
+  S3_ADMIN_AWS_SECRET_ACCESS_KEY=${S3_ADMIN_AWS_SECRET_ACCESS_KEY}
+  CORE_KEEPER_DISCORD_HOOK=${CORE_KEEPER_DISCORD_HOOK}"
 }
 
 RELEASE_NAME="core1"
 
-if [[ "${S3_ADMIN_AWS_ACCESS_KEY_ID}" == "" ]] || [[ "${S3_ADMIN_AWS_SECRET_ACCESS_KEY}" == "" ]]; then
+if [[ "${S3_ADMIN_AWS_ACCESS_KEY_ID}" == "" ]] \
+  || [[ "${S3_ADMIN_AWS_SECRET_ACCESS_KEY}" == "" ]] \
+  || [[ "${CORE_KEEPER_DISCORD_HOOK}" == "" ]]
+then
   usage
   exit 1
 fi
@@ -15,7 +20,7 @@ helm upgrade \
   --install \
   --set "aws_access_key_id=${S3_ADMIN_AWS_ACCESS_KEY_ID}" \
   --set "aws_secret_access_key=${S3_ADMIN_AWS_SECRET_ACCESS_KEY}" \
-  --set "hosted_zone_id=${S3_ADMIN_AWS_HOSTED_ZONE_ID}" \
+  --set "discord_hook=${CORE_KEEPER_DISCORD_HOOK}" \
   --values ./values.yaml \
   ${RELEASE_NAME} \
   .
